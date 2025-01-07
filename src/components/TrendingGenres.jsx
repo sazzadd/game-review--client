a480e5714c8e5b6d5c57ca72c95377cb3c652b5c
sazzad <sazzadador@gmail.com>
2025-01-07 15:24:35 +0600

    fix genere design issue

--- src/components/TrendingGenres.jsx
+++ src/components/TrendingGenres.jsx
@@ -4,28 +4,24 @@ const TrendingGenres = () => {
       name: "Action",
       description:
         "Thrilling games packed with intense action and epic adventures.",
-      image:
-        "https://i.ibb.co.com/xjSjWY5/images.jpg",
+      image: "https://i.ibb.co.com/xjSjWY5/images.jpg",
     },
     {
       name: "RPG",
       description:
         "Dive into immersive worlds and character-driven storytelling.",
-      image:
-        "https://i.ibb.co.com/x3Dmbkw/hq720.jpg",
+      image: "https://i.ibb.co.com/x3Dmbkw/hq720.jpg",
     },
     {
       name: "Simulation",
       description: "Experience realistic gameplay with simulation-style games.",
-      image:
-        "https://i.ibb.co.com/L5dckvC/images-1.jpg",
+      image: "https://i.ibb.co.com/L5dckvC/images-1.jpg",
     },
     {
       name: "Adventure",
       description:
         "Play your favorite Adventure with realistic mechanics and visuals.",
-      image:
-        "https://i.ibb.co.com/XzVCQ5S/images-2.jpg",
+      image: "https://i.ibb.co.com/XzVCQ5S/images-2.jpg",
     },
   ];
 
@@ -39,25 +35,27 @@ const TrendingGenres = () => {
           Discover the most popular genres and explore reviews tailored to your
           gaming style.
         </p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-          {genres.map((genre, index) => (
-            <div
-              key={index}
-              className="bg-gray-800 rounded-lg shadow-md overflow-hidden hover:scale-105 transition-transform duration-300"
-            >
-              <img
-                src={genre.image}
-                alt={genre.name}
-                className="w-full h-40 object-cover"
-              />
-              <div className="p-4">
-                <h3 className="text-xl font-semibold text-yellow-400 mb-2">
-                  {genre.name}
-                </h3>
-                <p className="text-gray-300">{genre.description}</p>
+        <div className="w-10/12 mx-auto">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+            {genres.map((genre, index) => (
+              <div
+                key={index}
+                className="bg-gray-800 rounded-lg shadow-md overflow-hidden hover:scale-105 transition-transform duration-300"
+              >
+                <img
+                  src={genre.image}
+                  alt={genre.name}
+                  className="w-full h-40 object-cover"
+                />
+                <div className="p-4">
+                  <h3 className="text-xl font-semibold text-yellow-400 mb-2">
+                    {genre.name}
+                  </h3>
+                  <p className="text-gray-300">{genre.description}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </section>
